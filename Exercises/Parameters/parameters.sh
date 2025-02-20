3195579fdee8c6210d5009a99bf5472d176ba714
Adam Fox <adam.d.humphreys93@gmail.com>
2025-02-20 04:08:35 +0000

    Add explanation of parameters and create example of parameters in relation to scripts

---
+++ Exercises/Parameters/parameters.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+echo "####################################"
+
+echo "This is parameter 1:  $1"
+echo "This is parameter 2:  $2"
+echo "This is parameter 3:  $3"
+echo "This is parameter 4:  $4"
+echo "This is parameter 5:  $5"
+
+echo "The number of parameters we have is: $# "
+echo "These are our parameters: $@ "
